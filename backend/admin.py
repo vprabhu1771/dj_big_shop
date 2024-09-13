5a59391d2b38ae13508121e020f4970974e4e130
vprabhu1771 <vprabhu1771@gmail.com>
2024-09-13 18:54:54 +0530

    Brand Model and Admin

--- backend/admin.py
+++ backend/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from django.utils.html import format_html
 
 from backend.forms import CustomerUserCreationForm, CustomerUserChangeForm
-from backend.models import CustomUser, NewsLetter, Label, Tag, Discount
+from backend.models import CustomUser, NewsLetter, Label, Tag, Discount, Brand
 
 
 # Register your models here.
@@ -65,3 +65,14 @@ class DiscountAdmin(admin.ModelAdmin):
     list_display = ('type', 'start_date', 'end_date', 'never_expired')
 
 admin.site.register(Discount, DiscountAdmin)
+
+class BrandAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'image_path','image_tag')
+
+    def image_tag(self, obj):
+        return  format_html('<img src="{}" width="150" height="150" />'.format(obj.image_path.url))
+
+    image_tag.short_description = 'Image'
+
+admin.site.register(Brand, BrandAdmin)
--- backend/models.py
+++ backend/models.py
@@ -129,3 +129,23 @@ class Discount(models.Model):
 
     class Meta:
         db_table = 'discount'
+
+class BrandStatus(models.TextChoices):
+    PUBLISHED = 'PUBLISHED', _('PUBLISHED')
+    DRAFT = 'DRAFT', _('DRAFT')
+    PENDING = 'PENDING', _('PENDING')
+
+class Brand(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name= models.CharField(max_length=255)
+    image_path= models.ImageField(upload_to='brands', null=True, blank=True, default='No_image_available.jpg')
+    status= models.CharField(
+        max_length=255,
+        choices=BrandStatus.choices,
+        default=BrandStatus.PUBLISHED
+    )
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        db_table = 'brand'
