481f8ad94edfaa1bae48f1fb66e82d8ea38d4f7b
vprabhu1771 <vprabhu1771@gmail.com>
2024-09-13 19:11:35 +0530

    OrderItem Model

--- backend/models.py
+++ backend/models.py
@@ -425,3 +425,23 @@ class Order(models.Model):
 
     class Meta:
         db_table = 'order'
+
+# OrderItem
+class OrderItem(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    # order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    # order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, related_name='order_items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    qty = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.IntegerField()
+
+    # item_total = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.order} {self.product.name} {self.qty} {self.unit_price} {self.amount}"
+
+    class Meta:
+        db_table = 'orderitem'
