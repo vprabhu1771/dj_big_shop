ebbbff8ff6fd650fedc7f62b272062b0cfed7ab9
vprabhu1771 <vprabhu1771@gmail.com>
2024-10-05 15:16:56 +0530

    Login UI Added

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 
-from frontend.views import forget_password, home, login, register, about, account, show
+from frontend.views import forget_password, home, auth_login, register, about, account, show
 
 urlpatterns = [
     path('', home, name='home'),
     path('product/<int:product_id>/', show, name='show'),  # Product detail page
     # path('/', home, name='home'),
-    path('login', login, name='login'),
+    path('login', auth_login, name='login'),
     path('register', register, name='register'),
     path('forget_password', forget_password),
     path('account', account, name='account'),
     path('about', about, name='about'),
+
 ]
