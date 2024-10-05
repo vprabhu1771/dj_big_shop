590659933dab378f433277195023e8d5205b1223
vprabhu1771 <vprabhu1771@gmail.com>
2024-10-05 15:06:58 +0530

    CORS Added

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,13 @@ ALLOWED_HOSTS = [
     '127.0.0.1'
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://192.168.1.122:8000",  # Add this if needed for your IP
+]
+
 
 # Application definition
 
@@ -57,6 +64,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
