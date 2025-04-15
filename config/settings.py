893e6e045ec6e383d11dfe76756ccbc585a4c3b2
vprabhu1771 <vprabhu1771@gmail.com>
2025-04-15 19:07:43 +0530

    GMail SMTP added

--- config/settings.py
+++ config/settings.py
@@ -172,3 +172,18 @@ LOGIN_REDIRECT_URL = '/'  # Change this to where you want users to go after logg
 
 # URL to redirect to after logout (optional)
 LOGOUT_REDIRECT_URL = '/'  # Change this to where you want users to go after logging out
+
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
